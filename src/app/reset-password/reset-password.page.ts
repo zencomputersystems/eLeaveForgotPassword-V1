b3884c3bf79c8829e16a435bd9c5c05cc3332176
Hanani Rafei <hanani@zen.com.my>
2020-08-17 18:36:33 +0800

    add spinner in reset password, update background height to fill available space, update checking for reset password

--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -28,13 +28,12 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
  * @implements {OnInit}
  */
 @Component({
-  selector: 'app-reset-password',
-  templateUrl: './reset-password.page.html',
-  styleUrls: ['./reset-password.page.scss'],
+  selector: "app-reset-password",
+  templateUrl: "./reset-password.page.html",
+  styleUrls: ["./reset-password.page.scss"],
 })
 export class ResetPasswordPage implements OnInit {
-
-  /** 
+  /**
    * This method is to bind current token value
    * @private
    * @memberof ResetPasswordPage
@@ -62,7 +61,7 @@ export class ResetPasswordPage implements OnInit {
   public confirmNewPassword;
 
   /**
-   * form group 
+   * form group
    * @type {FormGroup}
    * @memberof ResetPasswordPage
    */
@@ -90,6 +89,9 @@ export class ResetPasswordPage implements OnInit {
 
   public image: string = "../../assets/icon/beesuite.png";
 
+  public resetSpinWait = false;
+
+  public errorMsg = null;
   /**
    *Creates an instance of ResetPasswordPage.
    * @param {ActivatedRoute} resetPassRoute This property is to get methods from ActivatedRoute
@@ -105,10 +107,13 @@ export class ResetPasswordPage implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router
   ) {
-    this.myForm = this.formBuilder.group({
-      newPass: ['', [Validators.required]],
-      confirmPass: ['']
-    }, { validator: this.checkPasswords });
+    this.myForm = this.formBuilder.group(
+      {
+        newPass: ["", [Validators.required]],
+        confirmPass: [""],
+      },
+      { validator: this.checkPasswords }
+    );
   }
 
   /**
@@ -116,8 +121,8 @@ export class ResetPasswordPage implements OnInit {
    * @memberof ResetPasswordPage
    */
   ngOnInit() {
-    this.currToken = this.resetPassRoute.snapshot.paramMap.get('token');
-    this.currLoginType = this.resetPassRoute.snapshot.paramMap.get('loginType');
+    this.currToken = this.resetPassRoute.snapshot.paramMap.get("token");
+    this.currLoginType = this.resetPassRoute.snapshot.paramMap.get("loginType");
   }
 
   /**
@@ -126,10 +131,11 @@ export class ResetPasswordPage implements OnInit {
    * @returns
    * @memberof ResetPasswordPage
    */
-  checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+  checkPasswords(group: FormGroup) {
+    // here we have the 'passwords' group
     let pass = group.controls.newPass.value;
     let confirmPass = group.controls.confirmPass.value;
-    return pass === confirmPass ? null : { notSame: true }
+    return pass === confirmPass ? null : { notSame: true };
   }
 
   /**
@@ -144,19 +150,42 @@ export class ResetPasswordPage implements OnInit {
       password: window.btoa(this.myForm.controls.confirmPass.value),
       tokenId: this.currToken,
     };
-    this.resetPassApi.patchInvitation(data).subscribe(res => {
-      console.log(res);
-      if (res.status) {
-        this.resetPassInfoPopup.alertPopup('Password is updated successfully. Please login to your account', 'alert-success');
-        setTimeout(() => {
-          window.location.href = ENV.URL_EUSR;
-        }, 3000);
-      } else {
-        this.resetPassInfoPopup.alertPopup(res.message, 'alert-error');
+    this.resetSpinWait = true;
+    this.errorMsg = null;
+    this.resetPassApi.patchInvitation(data).subscribe(
+      (res) => {
+        console.log(res);
+        console.log(res[0].TOKEN_GUID);
+        // console.log(res.response.statusCode );
+        this.resetSpinWait = false;
+        if (
+          res.response.statusCode === 400 &&
+          res.response.statusCode !== undefined
+        ) {
+          // this.errorMsg = res.response.error + ". " + res.response.message;
+          this.resetPassInfoPopup.alertPopup(
+            res.response.error + ". " + res.response.message,
+            "alert-error"
+          );
+        }
+        if (res[0].TOKEN_GUID !== null && res[0].TOKEN_GUID !== undefined) {
+          this.errorMsg = null;
+          this.resetPassInfoPopup.alertPopup(
+            "Password is updated successfully. Please login to your account",
+            "alert-success"
+          );
+          setTimeout(() => {
+            // window.location.href = ENV.URL_EUSR;
+          }, 3000);
+        }
+      },
+      (err) => {
+        this.resetPassInfoPopup.alertPopup(
+          JSON.parse(err._body).message,
+          "alert-error"
+        );
       }
-    }, err => {
-      this.resetPassInfoPopup.alertPopup(JSON.parse(err._body).message, 'alert-error');
-    });
+    );
   }
 
   /**
@@ -164,15 +193,24 @@ export class ResetPasswordPage implements OnInit {
    * @memberof ResetPasswordPage
    */
   activateUser() {
-    this.resetPassApi.getInvitation(this.currToken).subscribe(res => {
-      console.log(res)
-      this.resetPassInfoPopup.alertPopup('Your account is activated successfully. Please login to your account', 'alert-success');
-      setTimeout(() => {
-        window.location.href = ENV.URL_EUSR;
-      }, 3000);
-    }, fail => {
-      this.resetPassInfoPopup.alertPopup(JSON.parse(fail._body).message, 'alert-error');
-    });
+    this.resetPassApi.getInvitation(this.currToken).subscribe(
+      (res) => {
+        console.log(res);
+        this.resetPassInfoPopup.alertPopup(
+          "Your account is activated successfully. Please login to your account",
+          "alert-success"
+        );
+        setTimeout(() => {
+          window.location.href = ENV.URL_EUSR;
+        }, 3000);
+      },
+      (fail) => {
+        this.resetPassInfoPopup.alertPopup(
+          JSON.parse(fail._body).message,
+          "alert-error"
+        );
+      }
+    );
   }
 
   /**
@@ -181,20 +219,26 @@ export class ResetPasswordPage implements OnInit {
    * @memberof ResetPasswordPage
    */
   requetResetPassword(encryptPass) {
-    this.resetPassApi.patchApiWoHeader('/api/forgot-password',
-      { tokenId: this.currToken, password: encryptPass })
-      .subscribe(
-        data => {
-          if (data.response === undefined) {
-            this.resetPassInfoPopup.alertPopup('Password is successfully updated. Please login to your account', 'alert-success');
-            setTimeout(() => {
-              window.location.href = data[0].HTTP_REFERER;
-            }, 2500);
-          } else {
-            this.resetPassInfoPopup.alertPopup(data.response.message, 'alert-error');
-          }
+    this.resetPassApi
+      .patchApiWoHeader("/api/forgot-password", {
+        tokenId: this.currToken,
+        password: encryptPass,
+      })
+      .subscribe((data) => {
+        if (data.response === undefined) {
+          this.resetPassInfoPopup.alertPopup(
+            "Password is successfully updated. Please login to your account",
+            "alert-success"
+          );
+          setTimeout(() => {
+            window.location.href = data[0].HTTP_REFERER;
+          }, 2500);
+        } else {
+          this.resetPassInfoPopup.alertPopup(
+            data.response.message,
+            "alert-error"
+          );
         }
-      );
+      });
   }
-
 }
